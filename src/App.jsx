e96a4d5590494153fc5248b852a1a2aafd7b1c83
Rony Nofrianto <novicalz@gmail.com>
2026-01-06 20:15:26 +0700

    feat: replace SongForm with SongFormBaru component in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import ChordDisplay from './components/ChordDisplay';
 import YouTubeViewer from './components/YouTubeViewer';
 import AutoScroll from './components/AutoScroll';
-import SongForm from './components/SongForm';
+import SongFormBaru from './components/SongFormBaru';
 import SetListManager from './components/SetListManager';
 import { initialSongs, initialSetLists } from './data/songs';
 import './App.css';
@@ -415,7 +415,7 @@ function App() {
       </div>
       
       {showSongForm && (
-        <SongForm
+        <SongFormBaru
           song={editingSong}
           onSave={handleSaveSong}
           onCancel={() => {
